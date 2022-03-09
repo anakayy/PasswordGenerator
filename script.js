df814610ca76dd635884a0eb3bdaf52b5cfeb0ad
Anousone <aninthirath@gmail.com>
2022-03-08 21:58:55 -0700

    added arrays

--- script.js
+++ script.js
@@ -8,13 +8,9 @@ function writePassword() {
 
   passwordText.value = password;
 
-  var chars = "1234AbCdE";
-  var passwordLength = 8;
-  var password = "";
-
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber + 1);
-}
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
